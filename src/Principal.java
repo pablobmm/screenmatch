95a493c4bcae65183667bea093112e9f6b810684
Pablo <148821043+pablobmm@users.noreply.github.com>
2025-10-29 22:41:00 -0300

    feat: criando métodos que modificam valores

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,10 @@
+import br.com.alura.screenmatch.modelos.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-        filme1.nome = "O poderoso chefão";
-        filme1.anoDeLancamento = 1970;
-        filme1.duracaoEmMinutos = 180;
-
+        filme1.setNome("O poderoso chefão");
+        filme1.setAnoDeLancamento(1970);
+        filme1.setDuracaoEmMinutos(180);
         filme1.exibeFichaTecnica();
         filme1.avalia(8);
         filme1.avalia(5);
